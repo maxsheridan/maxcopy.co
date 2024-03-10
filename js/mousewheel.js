6daed24d409031b44a59a26fe826bbb37d929e77
maxsheridan <max@maxsheridan.com>
2024-03-11 00:09:00 +0200

    Mouswheel js updated

--- js/mousewheel.js
+++ js/mousewheel.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
     function handleMouseWheel(e, delta) {
         if ($(window).width() > 1024) {
             // Apply horizontal scrolling for screens above 1024px wide
-            this.scrollLeft -= (delta * 5);
+            this.scrollLeft -= (delta * 30);
             e.preventDefault();
         }
         // For screens below 1025px, allow default vertical scrolling behavior
